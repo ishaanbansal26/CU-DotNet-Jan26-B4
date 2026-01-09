ebc0d40a7ff7049aa164bb96398978e1b8489ffd
ishaanbansal26 <ishanbansal2123@gmail.com>
2026-01-09 12:51:01 +0530

    ok

---
+++ TrainingPortal/TrainingPortal/Controllers/Training.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingPortal.Controllers
+{
+    public class Training : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
