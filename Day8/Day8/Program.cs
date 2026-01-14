f86cae770acb48c1f27713af3353535c208e9f29
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-01-14 14:44:04 +0530

    Day-8 Exercise Solution

---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Day8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("username|message :");
+            string[] msg = Console.ReadLine()!.Split('|',StringSplitOptions.RemoveEmptyEntries|StringSplitOptions.TrimEntries);
+            string userName = msg[0];
+            string message = msg[1].ToLower();
+            string std_message = "login successful";
+
+            //Business Rules
+            string status = "";
+            if (message.Contains("successful") && message.Equals(std_message)){
+                status = "LOGIN SUCCESS";
+                //Console.WriteLine(status);
+                
+            }
+            else if (message.Contains("successful")&& !message.Equals(std_message))
+            {
+                status = "LOGIN SUCCESS (CUSTOM MESSAGE)";
+                //Console.WriteLine(status);
+
+            }
+            else
+            {
+                status = "LOGIN FAILED";
+                //Console.WriteLine(status);
+                
+            }
+
+            Console.WriteLine($"{"User"}{":",5} {userName}");
+            Console.WriteLine($"{"Message"} {":",1} {message}");
+            Console.WriteLine($"{"Status"}{":",3} {status}");
+
+        }
+    }
+}
