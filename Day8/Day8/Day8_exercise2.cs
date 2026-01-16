8e05ebef8dcdfa485085330c86db8d156e2889b5
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-01-16 12:00:43 +0530

    Update Recently

---
+++ Day8/Day8/Day8_exercise2.cs
@@ -0,0 +1,39 @@
+﻿namespace Day8
+{
+    internal class Day8_exercise2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TransactionId#AccountHolderName#TransactionNarration");
+            string[] input = Console.ReadLine()!.Split('#',StringSplitOptions.RemoveEmptyEntries|StringSplitOptions.TrimEntries);
+            string tId = input[0];
+            string AccHolder = input[1];
+            string Transaction = input[2].ToLower().Trim(); 
+
+            string category = string.Empty;
+            if (!(Transaction.Contains("deposit") || Transaction.Contains("withdrawl") || Transaction.Contains("transfer")))
+            {
+                category = "NON-FINANCIAL TRANSACTION";
+            }
+
+            if(Transaction.Contains("deposit") || Transaction.Contains("withdrawl") || Transaction.Contains("transfer")
+                && Transaction.Equals("cash deposit successful"))
+            {
+                category = "STANDARD TRANSACTION";
+            }
+
+            if(Transaction.Contains("deposit") || Transaction.Contains("withdrawl") || Transaction.Contains("transfer")
+                && !Transaction.Equals("cash deposit successful"))
+            {
+                category = "CUSTOM TRANSACTION";
+            }
+
+
+            Console.WriteLine($"Transaction ID {":"} {tId}");
+            Console.WriteLine($"Account Holder {":"} {AccHolder}");
+            Console.WriteLine($"Narration {":",6} {Transaction,3}");
+            Console.WriteLine($"Category {":",7} {category}");
+        }
+        
+    }
+}
---
+++ Day9/Day9/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+
+namespace Day9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] arr = new decimal[7];
+            Console.WriteLine("Enter the sale for each day:");
+            decimal total = 0m;
+            decimal avg = 0m;
+            decimal highest = 0m;
+            decimal lowest = 0m;
+            decimal aboveAvg = 0m;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                arr[i] = decimal.Parse(Console.ReadLine()!);
+                if (arr[i] < 0)
+                {
+                    return;
+                }
+
+                total += arr[i];
+                avg = total / arr.GetLength(0);
+                if(i==0)
+                {
+                    lowest = arr[i];
+                    highest = arr[i];
+                }
+                else
+                {
+                    highest = Math.Max(highest, arr[i]);
+                    lowest = Math.Min(arr[0], arr[i]);
+                }
+                     
+                if(arr[i]>avg)
+                {
+                    aboveAvg++;
+                }
+            }
+
+            string[] category = new string[7];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 5000)
+                    category[i] = "Low";
+                else if (arr[i] > 5000 && arr[i] < 15000)
+                    category[i] = "Medium";
+                else
+                    category[i] = "High";
+            }
+
+
+            Console.WriteLine($"Total sales {":",9} {total}");
+            Console.WriteLine($"Average daily sales {":"} {avg:F2}");
+            Console.WriteLine($"highest sale {":",8} {highest}");
+            Console.WriteLine($"lowest sale {":",9} {lowest}");
+            Console.WriteLine($"days above Average {":",2} {aboveAvg}");
+
+            for (int i = 0; i <arr.GetLength(0); i++)
+            {
+                Console.WriteLine($"{"Day"}{i+1} {":"}{category[i]}");
+            }
+
+        }
+    }
+}
