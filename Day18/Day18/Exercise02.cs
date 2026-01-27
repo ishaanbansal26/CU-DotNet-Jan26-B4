5d1d151528c94098a417569bfb74cdffe9146221
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-01-27 16:09:55 +0530

    Recently Uploaded

---
+++ Day18/Day18/Exercise02.cs
@@ -0,0 +1,121 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace Day18
+{
+
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public decimal BasicSalary { get; set; }
+        public int ExperienceInYears{ get; set; }
+
+        public decimal annualSalary;
+        public int bonus;
+
+        public Employee(int id, string name, decimal salary, int years)
+        {
+            EmployeeId = id;
+            EmployeeName = name;        
+            BasicSalary = salary;
+            ExperienceInYears = years;
+        }
+
+        public decimal CalculateAnnualSalary()
+        {
+            Console.WriteLine("Employee ");
+            annualSalary = BasicSalary * 12;
+            return annualSalary;    
+        }
+
+        public string DisplayEmployeeDetails()
+        {
+            return $"{EmployeeId} {EmployeeName} {annualSalary} {ExperienceInYears}";
+        }
+    }
+
+    class PermanentEmployee : Employee
+    {
+        decimal houseRentAllowance;
+        decimal specialAllowance;
+        
+
+        public PermanentEmployee(int id, string name, decimal salary, int years)
+            : base(id, name, salary, years)
+        {
+            
+        }
+
+        public new decimal CalculateAnnualSalary()
+        {
+            houseRentAllowance = 0.2m * BasicSalary;
+            specialAllowance = 0.1m * BasicSalary;
+
+            if (ExperienceInYears >= 5)
+            {
+                bonus = 50000;
+            }
+            Console.WriteLine("Parent Employee");
+            return BasicSalary * 12+ houseRentAllowance+ specialAllowance
+                   + bonus;
+        }
+    }
+
+    class ContactEmployee : Employee
+    {
+        public int ContactDurationInMonths { get; set; }
+
+        
+        public ContactEmployee(int id, string name, decimal salary, int years)
+            : base(id, name, salary, years)
+        {
+            
+        }
+
+        public new decimal CalculateAnnualSalary()
+        {
+            annualSalary = BasicSalary * 12;
+            if (ContactDurationInMonths >= 12)
+            {
+                bonus = 30000;
+            }
+            Console.WriteLine("Contact Employee");
+            return annualSalary + bonus;
+        }
+    }
+
+    class InternEmployee : Employee
+    {
+        public InternEmployee(int id, string name, decimal salary, int years)
+            : base(id, name, salary, years)
+        {
+            annualSalary = BasicSalary * 12;
+        }
+
+        public new decimal CalculateAnnualSalary()
+        {
+            Console.WriteLine("Intern Employee");
+            return annualSalary; 
+        }
+    }
+
+    internal class Exercise02
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(1,"raju",7650,5);
+            PermanentEmployee emp2 = new PermanentEmployee(1, "taju", 7650, 5);
+            ContactEmployee emp3 = new ContactEmployee(1, "kaju", 7650, 5)
+            {
+                ContactDurationInMonths = 12,
+            };
+
+            Console.WriteLine(emp1.CalculateAnnualSalary());
+            Console.WriteLine(emp2.CalculateAnnualSalary());
+            Console.WriteLine(emp3.CalculateAnnualSalary());
+            Console.WriteLine(emp1.DisplayEmployeeDetails());
+
+        }
+    }
+}
---
+++ Day18/Day18/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace InheritanceLearn
+{
+    class Loan
+    {
+        public string LoanNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal PrincipalAmount { get; set; }
+        public int TenureInYears { get; set; }
+
+
+
+        public Loan()
+        {
+            LoanNumber = string.Empty;
+            CustomerName = string.Empty;
+        }
+        public Loan(string loanNumber, string customerName, decimal principalAmount, int tenure)
+        {
+            LoanNumber = loanNumber;
+            CustomerName = customerName;
+            PrincipalAmount = principalAmount;
+            TenureInYears = tenure;
+        }
+
+        public string CalculateEMI()
+        {
+            double emi = ((double)(PrincipalAmount) * 10 * (double)TenureInYears) / 100d;
+            return Convert.ToString(emi);
+        }
+
+        public override string ToString()
+        {
+            return $"Loan Number : {LoanNumber} Customer Name : {CustomerName}";
+        }
+    }
+
+    class HomeLoan : Loan
+    {
+        public HomeLoan(string loanNumber, string customerName, decimal principalAmount, int tenure) :
+            base(loanNumber, customerName, principalAmount, tenure)
+        {
+
+        }
+
+        public new string CalculateEMI()
+
+        {
+            Console.WriteLine("home loan");
+            double emi = (((double)(PrincipalAmount) * 0.01) * 8 * (double)TenureInYears) / 100d;
+            return base.ToString() + $"EMI : {emi}";
+        }
+    }
+
+    class CarLoan : Loan
+    {
+        public CarLoan(string loanNumber, string customerName, decimal principalAmount, int tenure) :
+            base(loanNumber, customerName, principalAmount, tenure)
+        {
+
+        }
+        int insurance = 15000;
+        public new string CalculateEMI()
+        {
+            Console.WriteLine("car loan");
+            double emi = ((double)(PrincipalAmount + insurance) * 9 * (double)TenureInYears) / 100d;
+            return base.ToString() + $"EMI : {emi}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Loan[] lo =
+            {
+                new Loan("abc", "hig", 986, 2),
+                new HomeLoan("cad", "haag", 986, 2),
+                new HomeLoan("dfe", "poig", 988996, 2),
+                new CarLoan("ggy", "uig", 986, 2),
+            };
+
+            for (int i = 0; i < lo.Length; i++)
+            {
+                Console.WriteLine($"{lo[i]} {lo[i].CalculateEMI()}");
+            }
+
+
+
+
+        }
+    }
+}
