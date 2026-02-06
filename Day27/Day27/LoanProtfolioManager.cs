0e08212c94f11290fbeb1ed04499c14fcb350883
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-02-06 14:04:40 +0530

    Day27 Solution

---
+++ Day27/Day27/LoanProtfolioManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+
+namespace Day27
+{
+    public class Loan
+    {
+        public string ClientName { get; set; }
+        public double Principal { get; set; }
+        public double InterestRate { get; set; }
+
+    }
+    internal class LoanProtfolioManager
+    {
+        static void Main(string[] args)
+        {
+            string directory = @"..\..\..\";
+            if (!Directory.Exists(directory))
+                throw new DirectoryNotFoundException("Directory not found.");
+            string file = "loanDetails.csv";
+
+            string path = directory + file;
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                //string[] details =
+                //{
+                //"Raj,5500,5.5",
+                //"Lucky,7500,5.5",
+                //"Raj,9500,5.5"
+                //};
+                //foreach (string detail in details)
+                //{
+                //    sw.WriteLine(detail);
+                //}
+                Console.WriteLine("Input : ClientName, Prinicpal, InterestRate");
+                Console.WriteLine("Enter the deatils in CSV.");
+                string s = Console.ReadLine();
+                sw.WriteLine(s);
+            }
+            Console.WriteLine("CLIENT       PRINICIPAL         INTEREST       RISKLEVEL");
+            using (StreamReader sr = new StreamReader(path))
+            {
+                do
+                {
+                    try
+                    {
+                        string line = sr.ReadLine();
+                        if (line == null)
+                            break;
+                        string[] ar = line.Split(",");
+                        if (ar.Length < 3)
+                            continue;
+                        string name = ar[0];
+                        double prinicipal = double.Parse(ar[1]);
+                        double interest = double.Parse(ar[ar.Length - 1]);
+                        double totalInterest = (prinicipal * interest) / 100;
+                        string riskLevel = string.Empty;
+                        if (interest > 10)
+                            riskLevel = "HIGH";
+                        else if (interest < 5)
+                            riskLevel = "LOW";
+                        else
+                            riskLevel = "MEDIUM";
+
+                        Loan l = new Loan()
+                        {
+                            ClientName = name,
+                            Principal = prinicipal,
+                            InterestRate = interest
+                        };
+
+                        Console.WriteLine($"{l.ClientName,-15}   {l.Principal,-15} {totalInterest,-12}  {riskLevel,-15}");
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                } while (true);             
+            }
+        }
+    }
+}
---
+++ Day27/Day27/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Day27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string directory = @"..\..\..\";
+                if (!Directory.Exists(directory))
+                    throw new DirectoryNotFoundException("Directory not found.");
+                string file = "journal.txt";
+                
+                string path = directory + file;
+
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    Console.WriteLine("Enter the things you did today : ");
+                    do
+                    {
+                        string things = Console.ReadLine();
+                        if (things.Equals("exit"))
+                            break;
+                        sw.WriteLine(things);
+                    } while (true);
+                }
+
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    do
+                    {
+                        string line = sr.ReadLine();
+                        if (line == null)
+                            break;
+                        Console.WriteLine(line);
+                    } while (true);
+                }
+            }
+            catch(DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
