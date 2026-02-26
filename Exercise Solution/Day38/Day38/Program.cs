888ca97ff4b10e7db7840d9027875b50bb202e35
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-02-26 18:40:19 +0530

    Day38 Solution

---
+++ Exercise Solution/Day38/Day38/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace Day38
+{
+    internal class Program
+    {
+        static string VowelShiftCipher(string s)
+        {
+            char[] vow = { 'a', 'e', 'i', 'o', 'u' };
+            string c = "bcdfghjklmnpqrstvwxyz";
+            char[] cons = c.ToCharArray();
+
+            StringBuilder sb = new StringBuilder(s);
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (vow.Contains(sb[i]))
+                {
+                    int idx = Array.IndexOf(vow, sb[i]);
+                    sb[i] = vow[(idx + 1) % vow.Length];
+                }
+
+                if (cons.Contains(sb[i]))
+                {
+                    int idx = Array.IndexOf(cons, sb[i]);
+                    sb[i] = cons[(idx + 1) % cons.Length];
+                }
+            }
+            s = sb.ToString();
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string :");
+            string s = Console.ReadLine();
+            Console.WriteLine(VowelShiftCipher(s));
+        }
+    }
+}
