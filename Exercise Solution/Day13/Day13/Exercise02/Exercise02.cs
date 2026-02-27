01c6f00f2dc5cc44318cd9b79cfee85dc475c494
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-02-27 15:20:09 +0530

    Day 13 Solution

---
+++ Exercise Solution/Day13/Day13/Exercise02/Exercise02.cs
@@ -0,0 +1,38 @@
+﻿namespace Day13.Exercise02
+{
+    internal class Exercise02
+    {
+        static void DisplayLine()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+
+        static void DisplayLine(char ch)
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write($"{ch}");
+            }
+            Console.WriteLine();
+        }
+
+        static void DisplayLine(char ch, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+                DisplayLine();        // ------------------------
+                DisplayLine('+');     // ++++++++++++++++++++++++
+                DisplayLine('$', 60); // $$$$$$$$$$$$$$$$$$$$$$$$
+        }
+    }
+}
