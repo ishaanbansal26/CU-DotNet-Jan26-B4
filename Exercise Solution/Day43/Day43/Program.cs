497ffd985383268a9c40b714b78412afb96608d1
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-03-02 22:44:31 +0530

    Recently Uploaded

---
+++ Exercise Solution/Day43/Day43/Program.cs
@@ -0,0 +1,291 @@
+﻿using System.Globalization;
+using Day43;
+
+namespace Day43
+{
+    interface IRiskAssessable
+    {
+        string GetRiskCategory();
+    }
+    interface IReportable
+    {
+        string GenerateReportLine();
+    }
+
+    class InvalidFinancialDataException : Exception
+    {
+        public InvalidFinancialDataException(string message) : base(message)
+        {
+
+        }
+    }
+
+    abstract class FinancialInstrument
+    {
+        public string InstrumentID { get; set; }
+        public string Name { get; set; }
+
+        private string _currency;
+        public string Currency
+        {
+            get { return _currency; }
+            set { _currency = value; }
+        }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                if (value < 0)
+                    throw new InvalidFinancialDataException("Quantity should not be less than zero");
+                _quantity = value;
+            }
+        }
+
+        private decimal _purchasePrice;
+        public decimal PurchasePrice
+        {
+            get { return _purchasePrice; }
+            set
+            {
+                if (value < 0)
+                    throw new InvalidFinancialDataException("Price cannot be negative.");
+                _purchasePrice = value;
+            }
+        }
+
+        public decimal MarketPrice { get; set; }
+        public DateOnly PurchaseDate { get; set; }
+
+        public abstract decimal CalculateCurrentValue();
+
+        public virtual string GetInstrumentSummary()
+        {
+            return $"{InstrumentID} - {Name} - {Currency}";
+        }
+    }
+
+    class Equity : FinancialInstrument, IRiskAssessable, IReportable
+    {
+        public override decimal CalculateCurrentValue()
+        {
+            return Quantity * MarketPrice;
+        }
+
+        public string GetRiskCategory()
+        {
+            return "High";
+        }
+
+        public string GenerateReportLine()
+        {
+            return $"{InstrumentID} - Equity - {CalculateCurrentValue():C}";
+        }
+    }
+
+    class Bond : FinancialInstrument, IRiskAssessable, IReportable
+    {
+        public override decimal CalculateCurrentValue()
+        {
+            return Quantity * MarketPrice;
+        }
+
+        public string GetRiskCategory()
+        {
+            return "Medium";
+        }
+
+        public string GenerateReportLine()
+        {
+            return $"{InstrumentID} - Bond - {CalculateCurrentValue():C}";
+        }
+    }
+    class FixedDeposit : FinancialInstrument, IRiskAssessable
+    {
+        public override decimal CalculateCurrentValue()
+        {
+            return Quantity * MarketPrice;
+        }
+
+        public string GetRiskCategory()
+        {
+            return "Low";
+        }
+    }
+
+    class MutualFund : FinancialInstrument, IRiskAssessable
+    {
+        public override decimal CalculateCurrentValue()
+        {
+            return Quantity * MarketPrice;
+        }
+
+        public string GetRiskCategory()
+        {
+            return "Medium";
+        }
+    }
+    class Portfolio
+    {
+        public List<FinancialInstrument> instrument = new List<FinancialInstrument>();
+        Dictionary<string, FinancialInstrument> dict = new Dictionary<string, FinancialInstrument>();
+
+        public void AddInstrument(FinancialInstrument inst)
+        {
+            if (!dict.ContainsKey(inst.InstrumentID))
+            {
+                instrument.Add(inst);
+                dict.Add(inst.InstrumentID, inst);
+            }
+        }
+        public void RemoveInstrument(string id)
+        {
+            if (dict.ContainsKey(id))
+            {
+                instrument.Remove(dict[id]);
+            }
+        }
+        public decimal GetTotalPortfolioValue()
+        {
+            return instrument.Sum(x => x.CalculateCurrentValue());
+        }
+
+        public FinancialInstrument GetInstrumentByID(string id)
+        {
+            if (dict.ContainsKey(id))
+                return dict[id];
+            return null;
+        }
+    }
+    class Transaction
+    {
+        public int TransactionId { get; set; }
+        public string InstrumentId { get; set; }
+        public string Type { get; set; } 
+        public int Units { get; set; }
+        public DateOnly Date { get; set; }
+        public static void ProcessTransactions(Portfolio portfolio)
+        {
+            Transaction[] transactions =
+            {
+            new Transaction { TransactionId = 1, InstrumentId = "EQ001", Type = "Buy", Units = 10, Date = DateOnly.FromDateTime(DateTime.Now) },
+            new Transaction { TransactionId = 2, InstrumentId = "EQ001", Type = "Sell", Units = 5, Date = DateOnly.FromDateTime(DateTime.Now) }
+            };
+
+            List<Transaction> tList = transactions.ToList();
+
+            foreach (var t in tList)
+            {
+                var instrument = portfolio.GetInstrumentByID(t.InstrumentId);
+
+                if (instrument == null)
+                {
+                    Console.WriteLine("Instrument not found");
+                    continue;
+                }
+
+                if (t.Type == "Buy")
+                {
+                    instrument.Quantity += t.Units;
+                }
+                else if (t.Type == "Sell")
+                {
+                    if (t.Units <= instrument.Quantity)
+                        instrument.Quantity -= t.Units;
+                    else
+                        Console.WriteLine("Cannot sell more than owned");
+                }
+            }
+        }
+    }
+
+    class ReportGenerator
+    {
+        public static void GenerateFileReport(Portfolio portfolio)
+        {
+            string fileName = @"../../../PortfolioReport_.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileName))
+                {
+                    sw.WriteLine("PORTFOLIO REPORT");
+                    sw.WriteLine("Generated On: " + DateTime.Now);
+                    sw.WriteLine();
+
+                    var ordered = portfolio.instrument.OrderByDescending(i => i.CalculateCurrentValue());
+
+                    foreach (var inst in ordered)
+                    {
+                        sw.WriteLine(
+                            $"{inst.InstrumentID} | {inst.Name} | " +
+                            $"Qty: {inst.Quantity} | " +
+                            $"Current Value: {inst.CalculateCurrentValue().ToString("C")}"
+                        );
+                    }
+
+                    sw.WriteLine();
+                    var groups = portfolio.instrument.GroupBy(i => i.GetType().Name);
+
+                    foreach (var group in groups)
+                    {
+                        decimal totalInvestment = group.Sum(i => i.Quantity * i.PurchasePrice);
+                        decimal currentValue = group.Sum(i => i.CalculateCurrentValue());
+
+                        sw.WriteLine();
+                        sw.WriteLine($"Instrument Type: {group.Key}");
+                        sw.WriteLine($"Total Investment: {totalInvestment.ToString("C")}");
+                        sw.WriteLine($"Current Value: {currentValue.ToString("C")}");
+                        sw.WriteLine($"Profit/Loss: {(currentValue - totalInvestment).ToString("C")}");
+                    }
+
+                    sw.WriteLine();
+                    sw.WriteLine("Overall Portfolio Value: " + portfolio.GetTotalPortfolioValue().ToString("C"));
+                }
+
+                Console.WriteLine("File report generated successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Portfolio portfolio = new Portfolio();
+
+        portfolio.AddInstrument(new Equity()
+        {
+            InstrumentID = "EQ001",
+            Name = "INFY",
+            Currency = "INR",
+            Quantity = 100,
+            PurchasePrice = 1500,
+            MarketPrice = 1650,
+            PurchaseDate = DateOnly.FromDateTime(DateTime.Now)
+        });
+
+        portfolio.AddInstrument(new Bond()
+        {
+            InstrumentID = "BD001",
+            Name = "GovBond",
+            Currency = "INR",
+            Quantity = 50,
+            PurchasePrice = 1000,
+            MarketPrice = 1050,
+            PurchaseDate = DateOnly.FromDateTime(DateTime.Now)
+        });
+
+        Transaction.ProcessTransactions(portfolio);
+        ReportGenerator.GenerateFileReport(portfolio);
+        Console.ReadLine();
+    }
+}
+
+
---
+++ Exercise Solution/Day44/Day44/SAASArch.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+
+namespace Day44
+{
+    abstract class Subscriber : IComparable<Subscriber>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime JoinDate { get; set; }
+
+        public abstract decimal CalculateMonthlyBill();
+
+        public int CompareTo(Subscriber? other)
+        {
+            if (this.JoinDate == other.JoinDate)
+                return this.Name.CompareTo(other.Name);
+            return this.JoinDate.CompareTo(other.JoinDate);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var objId = obj as Subscriber;
+            return this.Id.Equals(objId?.Id);
+        }
+        protected Subscriber(int id, string name, DateTime date)
+        {
+            Id = id;
+            Name = name;
+            JoinDate = date;
+        }
+    }
+
+    class BusinessSubscriber : Subscriber
+    {
+        public decimal FixedRate { get; set; }
+        public decimal TaxRate { get; set; }
+        public BusinessSubscriber(int id, string name, DateTime joinDate,
+            decimal fixedRate, decimal taxRate)
+            : base(id, name, joinDate)
+        {
+            FixedRate = fixedRate;
+            TaxRate = taxRate;
+        }
+        public override decimal CalculateMonthlyBill()
+        {
+            return FixedRate * (1+TaxRate);
+        }
+    }
+    class ConsumerSubscriber : Subscriber
+    {
+        public decimal DataUsageGB { get; set; }
+        public decimal PricePerGB { get; set; }
+        public ConsumerSubscriber(int id, string name, DateTime joinDate,
+            decimal dataUsageGB, decimal pricePerGB)
+            : base(id, name, joinDate)
+        {
+            DataUsageGB = dataUsageGB;
+            PricePerGB = pricePerGB;
+        }
+        public override decimal CalculateMonthlyBill()
+        {
+            return DataUsageGB * PricePerGB;
+        }
+    }
+    class ReportGenerator
+    {
+        public static void PrintRevenueReporter(IEnumerable<Subscriber> subscribers)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{"ID"} {"Name"} {"JoinDate"} {"MonthlyBill"}");
+            foreach(var v in subscribers)
+            {
+                sb.AppendLine($"{v.Id} {v.Name} {v.JoinDate:dd-MM-yyyy} {v.CalculateMonthlyBill():N2}");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+    internal class SAASArch
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Subscriber> subscriber = new();
+            subscriber.Add("abc@gmail.com", new BusinessSubscriber(1, "Raj", new DateTime(2026, 01, 01),700m,0.25m));
+            subscriber.Add("def@gmail.com", new BusinessSubscriber(2, "Lucky", new DateTime(2026, 01,02),500m,0.22m));
+            subscriber.Add("ghi@gmail.com", new ConsumerSubscriber(3, "Hero", new DateTime(2026, 01, 03),5m,90m));
+            subscriber.Add("jkl@gmail.com", new ConsumerSubscriber(4, "Aman", new DateTime(2026, 01, 04),6m,80m));
+            subscriber.Add("pqr@gmail.com", new BusinessSubscriber(5, "Rakesh", new DateTime(2026, 01, 05), 700m, 0.25m));
+
+            Console.WriteLine(subscriber["abc@gmail.com"].Equals(subscriber["def@gmail.com"]));
+
+            var sortByBill = subscriber.Values.OrderByDescending(x => x.CalculateMonthlyBill()).ToList();
+            ReportGenerator.PrintRevenueReporter(sortByBill);
+        }
+    }
+}
