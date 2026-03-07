a53f161e2990b374ef4fbfc29c24b4fa80c8e5a2
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-03-07 10:05:30 +0530

    Recently Updated

---
+++ Assignments/Day50/Day50/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Day50
+{
+    class Stud
+    {
+        public int StudId { get; set; }
+        public string SName { get; set; }
+        public override bool Equals(object? obj)
+        {
+            var x = obj as Stud;
+            return this.StudId == x?.StudId && SName == x.SName;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StudId, SName);
+        }
+    }
+
+    class ManageStudent
+    {
+        Dictionary<Stud, int> dic = new();
+        public void AddStudent(Stud st, int marks)
+        {
+            if (dic.ContainsKey(st))
+            {
+                if (marks > dic[st])
+                {
+                    dic[st] = marks;
+                }
+            }
+            else
+            {
+                dic.Add(st, marks);
+            }
+        }
+        public void Display()
+        {
+            var display = dic.Select(s => new { s.Key.StudId, s.Key.SName, s.Value });
+            foreach (var v in display)
+            {
+                Console.WriteLine(v.StudId + v.SName + v.Value);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManageStudent m = new();
+            m.AddStudent(new Stud()
+            {
+                StudId = 1,
+                SName = "Raj"
+            }, 67);
+            m.AddStudent(new Stud()
+            {
+                StudId = 1,
+                SName = "Raj"
+            }, 38);
+            m.AddStudent(new Stud()
+            {
+                StudId = 2,
+                SName = "Rahul"
+            }, 90);
+            m.AddStudent(new Stud()
+            {
+                StudId = 3,
+                SName = "Rakesh"
+            }, 56);
+
+            m.Display();
+        }
+    }
+}
