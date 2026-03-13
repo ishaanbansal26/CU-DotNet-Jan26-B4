e9ba4f109d904b6c5fc02592966b3bd247f08b89
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-03-13 23:05:01 +0530

    Day 56 Solution

---
+++ Assignments/Day56/Day56/Day56/Controllers/LoanController.cs
@@ -0,0 +1,129 @@
+﻿using Day56.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day56.Controllers
+{
+    public class LoanController : Controller
+    {
+        private static List<Loan> _loans = [new()
+        {
+            Id=1,
+            LenderName="Ishaan",
+            BorrowerName="Bansal",
+            Amount=560d,
+            IsSettled=true
+        },
+        new()
+        {
+            Id=2,
+            LenderName="Rakesh",
+            BorrowerName="Aman",
+            Amount=8900,
+            IsSettled=false
+        },new()
+        {
+            Id=3,
+            LenderName="Lucy",
+            BorrowerName="Nikki",
+            Amount=1200,
+            IsSettled=true
+        }];
+        
+        
+        
+        
+        // GET: LoanController
+        public ActionResult Index()
+        {
+            return View(model: _loans);
+        }
+
+        // GET: LoanController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    var x = _loans.Find(i => i.Id == id);
+        //    return View(x);
+        //}
+
+        // GET: LoanController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoanController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Loan loan)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(loan);
+                }
+                _loans.Add(loan);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+            
+        }
+
+        // GET: LoanController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var x = _loans.Find(x => x.Id == id);
+            return View(x);
+        }
+
+        // POST: LoanController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Loan loan)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                {
+                    return View(loan);
+                }
+                var i = _loans.Find(x => x.Id == id);
+                i.BorrowerName = loan.BorrowerName;
+                i.LenderName = loan.LenderName;
+                i.Amount = loan.Amount;
+                i.IsSettled = loan.IsSettled;
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoanController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var x = _loans.Find(x => x.Id == id);
+            return View(x);
+        }
+
+        // POST: LoanController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id,Loan loan)
+        {
+            var l = _loans.Find(x => x.Id == id);
+
+            if (l != null)
+            {
+                _loans.Remove(l);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Assignments/Day56/Day56/Day56/Models/Loan.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day56.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Borrower Name")]
+        public string BorrowerName { get; set; }
+        public string LenderName { get; set; }
+        [Range(1,500000)]
+        public double Amount { get; set; }
+        public bool IsSettled { get; set; }
+    }
+}
