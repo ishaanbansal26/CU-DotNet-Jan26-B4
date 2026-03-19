15980682b5ce8637f525b9fe79c00e42cd939d2d
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-03-19 21:46:43 +0530

    Recently Uploaded

---
+++ Assignments/Day60/Day60/Day60/Data/PortfolioContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Day60.Models;
+
+namespace Day60.Data
+{
+    public class PortfolioContext : DbContext
+    {
+        public PortfolioContext (DbContextOptions<PortfolioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Day60.Models.Investment> Investment { get; set; } = default!;
+    }
+}
---
+++ Assignments/Day60/Day60/Day60/ViewModel/InvestmentCreateViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Day60.ViewModel
+{
+    public class InvestmentCreateViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ticker is required")]
+        [StringLength(10)]
+        [Display(Name = "Ticker Symbol")]
+        public string TickerSymbol { get; set; }
+
+        [Display(Name = "Asset Name")]
+        public string AssetName { get; set; }
+
+        [Required]
+        [Range(0.01, 1000000)]
+        [Display(Name = "Purchase Price")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Purchase Date")]
+        [DataType(DataType.Date)]
+        public DateTime PurchaseDate { get; set; }
+
+        [Display(Name = "Total Investment Value")]
+        public string TotalValue => (Price * Quantity).ToString("C");
+    }
+
+}
