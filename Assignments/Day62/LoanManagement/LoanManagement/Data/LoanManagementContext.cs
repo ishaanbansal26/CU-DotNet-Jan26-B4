5b7a818beca61c422619bbd412e38a47f0a97b31
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-03-20 11:56:45 +0530

    Day 62 Solution

---
+++ Assignments/Day62/LoanManagement/LoanManagement/Data/LoanManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LoanManagement.Models;
+
+namespace LoanManagement.Data
+{
+    public class LoanManagementContext : DbContext
+    {
+        public LoanManagementContext (DbContextOptions<LoanManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoanManagement.Models.Loan> Loan { get; set; } = default!;
+    }
+}
---
+++ Assignments/Day62/LoanManagement/LoanManagement/Models/Loan.cs
@@ -0,0 +1,11 @@
+﻿namespace LoanManagement.Models
+{
+    public class Loan
+    {
+        public int LoanId { get; set; }
+        public string BorrowerName { get; set; }
+        public decimal Amount { get; set; }
+        public int LoanTermMonths { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
