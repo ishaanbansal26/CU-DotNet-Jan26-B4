500b4e6d8f5f68f9cacf93d2b0f26e360ba95e15
Ishaan Bansal <ishanbansal2123@gmail.com>
2026-04-25 10:47:45 +0530

    Day 90 Solution Uploaded

---
+++ Assignments/Day90/Day90/TicketBookingSystem.TripService/TicketBookingSystem.TripService/Common/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketBookingSystem.TripService.Common
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
